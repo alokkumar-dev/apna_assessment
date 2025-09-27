735c2687a906b638257a2ad6e90d9199c3d27e74
alokkumar-dev <alokshakya2601@gmail.com>
2025-09-27 20:02:17 +0530

    loader bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ function AppContent() {
     <>
       {user && <Navbar />}
       <Routes>
-        <Route path="/" element={user ? <Navigate to="/dashboard" /> : <LoginPage />} />
+        <Route path="/" element={user ? <Navigate to="/profile" /> : <LoginPage />} />
         {/* <Route path="/register" element={<RegisterPage />} /> */}
         <Route path="/login" element={<LoginPage />} />
         <Route
